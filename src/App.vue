97a6d50581f7751d0e8395a3357815f941747bdc
Luis Reyes <lualreye@gmail.com>
2022-11-24 17:16:53 -0500

    feat: importing icons from material design

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="app-wrapper">
+    <Header />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Icons from '../components/Icons.vue'
+
+</script>
+
+<template>
+  <header class="header">
+    <img src="../assets/logo.png" alt="logo" class="logo">
+    <div class="icons-section">
+      <div class="icon-wrapper">
+        <Icons name="facebook" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  max-width: 1280px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+  margin: 0 auto;
+}
+.logo {
+  width: 45px;
+  height: 45px;
+}
+.icon-wrapper {
+  width: 32px;
+  height: 32px;
+  color: #3b5998;
+}
+</style>
